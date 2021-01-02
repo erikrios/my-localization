d60eba7f6484b101b2907395ed8647fefad7d77b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-02 19:30:48 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/mylocalization/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mylocalization/MainActivity.kt
@@ -1,11 +1,29 @@
 package io.erikrios.github.mylocalization
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.mylocalization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pokeCouont = 3
+        val hello = resources.getString(
+            R.string.hello_world, "Narendra Wicaksono", pokeCouont, "Yoza Aprilio"
+        )
+        binding.tvHello.text = hello
+
+        val songCOunt = 5
+        val pluralText =
+            resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCOunt, songCOunt)
+        binding.tvPlural.text = pluralText
+
+        binding.tvXliff.text = resources.getString(R.string.app_homeurl)
     }
 }
