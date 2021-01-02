ca52a3643b889fc8455262a1d8659f9f2d7eb374
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-02 19:33:04 +0700

    Override onCreateOptionsMenu and onOptionsItemSelected method

--- app/src/main/java/io/erikrios/github/mylocalization/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mylocalization/MainActivity.kt
@@ -1,6 +1,10 @@
 package io.erikrios.github.mylocalization
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.erikrios.github.mylocalization.databinding.ActivityMainBinding
 
@@ -25,5 +29,18 @@ class MainActivity : AppCompatActivity() {
         binding.tvPlural.text = pluralText
 
         binding.tvXliff.text = resources.getString(R.string.app_homeurl)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
